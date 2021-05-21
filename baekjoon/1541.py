40212e9c67490e7f4f002926f9ce37bbb76048fe
yuuun <ynkim31@naver.com>
2021-05-21 12:30:36 +0900

    greedy 0521

--- baekjoon/1541.py
+++ baekjoon/1541.py
@@ -1,1 +1,14 @@
-expon = input()
+#greedy algorithm
+expression = input()
+exp_arr = expression.split('-')
+ans = 0
+
+for e in map(int, exp_arr[0].split('+')):
+    ans += e
+
+for exp in exp_arr[1:]:
+    ex = exp.split('+')
+    for e in ex:
+        ans -= int(e)
+
+print(ans)
