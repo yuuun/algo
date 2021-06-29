8b4717d47413ef88a6a916e305c8a4f6626f74c2
yuuun <ynkim31@naver.com>
2021-06-29 17:19:12 +0900

    0629

--- baekjoon/7562.py
+++ baekjoon/7562.py
@@ -1,3 +1,4 @@
+from collections import deque
 n = int(input())
 d = [[2, -1], [2, 1], [-2, -1], [-2, 1],
     [1, 2], [1, -2], [-1, 2], [-1, -2]]
@@ -5,4 +6,17 @@ for _ in range(n):
     k = int(input())
     x, y = map(int, input().split())
     desx, desy = map(int, input().split())
-    
+    visited = [[0] * k for _ in range(k)]
+    queue = deque()
+    queue.append([x, y])
+    visited[x][y] = 1
+    while queue:
+        x, y = queue.popleft()
+        if x == desx and y == desy:
+            print(visited[x][y] - 1)
+            break
+        for dx, dy in d:
+            tx, ty = x + dx, y + dy
+            if 0 <= tx < k and 0 <= ty < k and visited[tx][ty] == 0:
+                queue.append([tx, ty])
+                visited[tx][ty] = visited[x][y] + 1
--- baekjoon/7569.py
+++ baekjoon/7569.py
@@ -1,3 +1,38 @@
-n, m, h = map(int, input().split())
-for _ in range(m):
-    
+from collections import deque
+m, n, h = map(int, input().split())
+adj = [[[0] * m for _ in range(n)] for _ in range(h)]
+
+q = deque()
+
+for k in range(h):
+    for j in range(n):
+        ad = list(map(int, input().split()))
+        adj[k][j] = ad
+        for idx, val in enumerate(ad):
+            if val == 1:
+                q.append([k, j, idx])
+        
+d = [[1, 0, 0], [-1, 0, 0], [0, 1, 0], 
+    [0, -1, 0], [0, 0, 1], [0, 0, -1]]
+
+while q:
+    z, y, x = q.popleft()
+    for dz, dy, dx in d:
+        tz, ty, tx = z + dz, y + dy, x + dx
+        if 0 <= tz < h and 0 <= ty < n and 0 <= tx < m:
+            if adj[tz][ty][tx] == 0:
+                adj[tz][ty][tx] = adj[z][y][x] + 1
+                q.append([tz, ty, tx])
+
+def print_val(adj):
+    max_val = 0
+    for ad in adj:
+        for a in ad:
+            if 0 in a:
+                return -1
+            for i in a:
+                if max_val < i:
+                    max_val = i
+    return max_val - 1
+
+print(print_val(adj))
