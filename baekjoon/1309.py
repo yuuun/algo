0848a23f8a1779d403544fb8fbfbfde3e90039b7
yuuun <ynkim31@naver.com>
2021-07-15 15:42:27 +0900

    0715

---
+++ baekjoon/1309.py
@@ -0,0 +1,9 @@
+n = int(input())
+dp = [[0] * 3 for _ in range(n + 1)]
+for i in range(3):
+    dp[1][i] = 1
+for i in range(2, n + 1):
+    dp[i][0] = dp[i - 1][1] + dp[i - 1][2] % 9901
+    dp[i][1] = dp[i - 1][0] + dp[i - 1][2] % 9901
+    dp[i][2] = dp[i - 1][0] + dp[i - 1][1] + dp[i - 1][2] % 9901
+print(sum(dp[n]) % 9901)
