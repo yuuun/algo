36646c40e99c010e7a0c17fc78d0a86508101693
yuuun <ynkim31@naver.com>
2021-07-18 13:43:20 +0900

    0718

---
+++ baekjoon/5582.py
@@ -0,0 +1,11 @@
+ans = 0
+a = input()
+b = input()
+dp = [[0] * (len(b) + 1) for _ in range(len(a) + 1)]
+
+for i in range(1, len(a) + 1):
+    for j in range(1, len(b) + 1):
+        if a[i - 1] == b[j - 1]:
+            dp[i][j] = dp[i - 1][j - 1] + 1
+            ans = max(dp[i][j], ans)
+print(ans)
