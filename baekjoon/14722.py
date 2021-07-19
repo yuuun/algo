f7baf06be2bb2603fdc4e8e41b851c155b029379
yuuun <ynkim31@naver.com>
2021-07-19 18:17:51 +0900

    0719

--- baekjoon/14722.py
+++ baekjoon/14722.py
@@ -1,29 +1,30 @@
+#TBD
 n = int(input())
 adj = [list(map(int, input().split())) for _ in range(n)]
-dp = [[0] * (n + 1) for _ in range(n + 1)]
+dp = [[0] * n for _ in range(n)]
 
 dic = {0: 2, 1: 0, 2: 1}
-dp[1][1] = 1
-for j in range(2, n + 1):
-    if dic[adj[0][j - 1]] == adj[0][j - 2]:
-        dp[1][j - 1] = dp[1][j - 2] + 1
+dp[0][0] = 1
+for i in range(1, n):
+    if dic[adj[i][0]] == adj[i - 1][0]:
+        dp[i][0] = dp[i - 1][0] + 1
     else:
-        dp[1][j - 1] = dp[1][j - 2]
-
-for i in range(2, n + 1):
-    if dic[adj[i - 1][0]] == adj[i - 2][0]:
-        dp[i - 1][1] = dp[i - 2][1] + 1
+        dp[i][0] = dp[i - 1][0]
+for j in range(1, n):
+    if dic[adj[0][j]] == adj[0][j - 1]:
+        dp[0][j] = dp[0][j - 1] + 1
     else:
-        dp[i - 1][1] = dp[i - 2][1]
+        dp[0][j] = dp[0][j - 1]
 
 max_val = 0
-for i in range(2, n + 1):
-    for j in range(2, n + 1):
+for i in range(1, n):
+    for j in range(1, n):
         tmp = [dp[i][j - 1], dp[i - 1][j]]
-        if dic[adj[i - 1][j - 1]] == adj[i - 1][j - 2]:
+        val = dic[adj[i][j]]
+        if val == adj[i][j - 1]:
             tmp.append(dp[i][j - 1] + 1)
-        if dic[adj[i - 1][j - 1]] == adj[i - 2][j - 1]:
+        if val == adj[i - 1][j]:
             tmp.append(dp[i - 1][j] + 1)
         dp[i][j] = max(tmp)
         max_val = max(dp[i][j], max_val)
-print(max_val)
+print(dp)
