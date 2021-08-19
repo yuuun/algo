24eae290706c910bd29b9fb37dc5fc0f1f979ed0
yuuun <ynkim31@naver.com>
2021-08-19 15:03:57 +0900

    0819

--- baekjoon/10816.py
+++ baekjoon/10816.py
@@ -1,4 +1,4 @@
-#시간초과
+#a list에 대하여 각각이 몇개 있는지 저장하는 방식으로 dictionary를 정의해야 함.
 n, a = int(input()), sorted(list(map(int, input().split())))
 m, b = int(input()), list(map(int, input().split()))
 
@@ -7,17 +7,16 @@ def binary_search(bb, start, end):
         return 0
     m = (start + end) // 2
     if bb == a[m]:
-        return 1
+        return a[start: end + 1].count(bb)
     elif bb < a[m]:
         return binary_search(bb, start, m - 1)
     else:
         return binary_search(bb, m + 1, end)
 
-ans = []
-for bb in b:
-    tmp = binary_search(bb, 0, n - 1)
-    if tmp == 0:
-        ans.append(tmp)
-    else:
-        ans.append(a.count(bb))
-print(' '.join(map(str, ans)))
+n_dic = {}
+
+for aa in a:
+    if aa not in n_dic:
+        n_dic[aa] = binary_search(aa, 0, n - 1)
+
+print(' '.join(str(n_dic[x]) if x in n_dic else '0' for x in b))
