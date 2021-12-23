ce627c9f5cea5c8dda114c56a989257f53cd825b
yuuun <ynkim31@naver.com>
2021-12-23 12:46:06 +0900

    day3 dp

---
+++ leetcode/deleteAndEarn.py
@@ -0,0 +1,15 @@
+from collections import Counter
+class Solution:
+    def deleteAndEarn(self, nums: List[int]) -> int:
+        if len(nums) == 0:
+            return 0
+        
+        sum_nums = [0] * (max(nums) + 1)
+        for num in nums:
+            sum_nums[num] += num
+        
+        dp = [0] * (max(nums) + 1)
+        for idx, cnt in enumerate(sum_nums[:-1]):
+            dp[idx + 1] += max(dp[idx - 1], dp[idx - 2]) + sum_nums[idx + 1]
+            
+        return max(dp[-1], dp[-2])
