57042bdffe3ed0bb99f20dac3e36019f0130263c
yuuun <ynkim31@naver.com>
2021-12-30 15:57:37 +0900

    dij

--- baekjoon/1238.py
+++ baekjoon/1238.py
@@ -2,20 +2,21 @@ import heapq
 import sys
 from collections import defaultdict
 INF = sys.maxsize
-n, m = map(int, input().split())
+
+n, m, x = map(int, input().split())
 route = defaultdict(list)
 
 for _ in range(m):
     s, e, c = map(int, input().split())
     route[s].append([e, c])
-    route[e].append([s, c])
+
 
 def get_min(st):
     queue = []
-    first = ['-' for _ in range(n + 1)]
     heapq.heappush(queue, (st, 0))
     dist = [INF] * (n + 1)
-    dist[st] = 0
+    # dist[st] = 0
+    
     while queue:
         cur, dis = heapq.heappop(queue)
         if dist[cur] < dis:
@@ -23,17 +24,15 @@ def get_min(st):
         for ne, nd in route[cur]:
             cost = dis + nd
             if dist[ne] > cost:
-                first[ne] = cur
                 dist[ne] = cost
                 heapq.heappush(queue, (ne, cost))
-    return first[1:]
+    return dist[1:]
+
+distance = []
+for i in range(1, n + 1):
+    distance.append(get_min(i))
 
 ans = []
-for i in range(1, n + 1):
-    
-    ans.append(get_min(i))
-
-for i in range(len(ans)):
-    for j in range(len(ans)):
-        print(str(ans[j][i]), end=' ')
-    print()
+for i in range(n):
+    ans.append(distance[i][x] + distance[x][i])
+print(max(ans))
