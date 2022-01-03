4edd5c736c58fa048be307bbf463aafcfcc09ab9
yuuun <ynkim31@naver.com>
2022-01-03 13:22:30 +0900

    simulation

--- baekjoon/1107.py
+++ baekjoon/1107.py
@@ -1,17 +1,16 @@
-#TBD
 n = int(input())
 m = int(input())
-print(list(map(int, input().split())))
-import pdb; pdb.set_trace()
-but = [i for i in range(10)].remove(list(map(int, input().split())))
+
+but = list(map(str, input().split()))
 
 min_cnt = abs(n - 100)
 
 for nu in range(1000001):
     num = str(nu)
     for i in range(len(num)):
-        if num[i] not in but:
+        if num[i] in but:
             break
         elif i == len(num) - 1:
             min_cnt = min(min_cnt, len(num) + abs(n - nu))
+            
 print(min_cnt)
