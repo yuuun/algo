c1e7b581a11bd3603225999ad8fbc99555d52124
yuuun <ynkim31@naver.com>
2022-01-31 22:07:27 +0900

    dp

--- baekjoon/10164.py
+++ baekjoon/10164.py
@@ -2,6 +2,10 @@ n, m, k = map(int, input().split())
 x, y = k // m, k % m
 if y != 0:
     y -= 1
+else:
+    if x != 0:
+        x -= 1
+        y = m - 1
 
 def cnt(r, c):
     maps = [[1] * c for _ in range(r)]
@@ -9,7 +13,6 @@ def cnt(r, c):
     for i in range(1, r):
         for j in range(1, c):
             maps[i][j] = maps[i][j - 1] + maps[i - 1][j]
-    # print(maps)
     return maps[-1][-1]
 
 print(cnt(x + 1, y + 1) * cnt(n - x, m - y))
