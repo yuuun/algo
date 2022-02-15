f4b1d52476d7db6bf7e9a772e94cab4f89ce0a88
yuuun <ynkim31@naver.com>
2022-02-16 01:48:27 +0900

    samsung

---
+++ baekjoon/14499.py
@@ -0,0 +1,35 @@
+n, m, x, y, k = map(int, input().split())
+maps = []
+for _ in range(n):
+    maps.append(list(map(int, input().split())))
+
+directions = list(map(int, input().split()))
+
+dxy = [[], [0, 1], [0, -1], [-1, 0], [1, 0]]    # 동, 서, 북, 남
+dice = [0, 0, 0, 0, 0, 0, 0] # idx를 위해서 앞에 허수 붙여줌 
+
+def change_dice(t):
+    if t == 1: # 동 - 2, 5 그대로
+        dice[1], dice[3], dice[4], dice[6] = dice[4], dice[1], dice[6], dice[3]
+    elif t == 2: # 서 - 2, 5 그대로
+        dice[1], dice[3], dice[4], dice[6] = dice[3], dice[6], dice[1], dice[4]
+    elif t == 3: # 북 - 3, 4 그대로
+        dice[1], dice[2], dice[5], dice[6] = dice[5], dice[1], dice[6], dice[2]
+    else: # 남 - 3, 4 그대로
+        dice[1], dice[2], dice[5], dice[6] = dice[2], dice[6], dice[1], dice[5]
+
+for t in directions:
+    dx, dy = dxy[t][0], dxy[t][1]
+    x, y = x + dx, y + dy
+    if x < 0 or x >= n or y < 0 or y >= m:
+        x = x - dx
+        y = y - dy
+        continue
+
+    change_dice(t)
+    if maps[x][y] == 0:
+        maps[x][y] = dice[6]
+    else:
+        dice[6] = maps[x][y]
+        maps[x][y] = 0
+    print(dice[1])
--- baekjoon/21610.py
+++ baekjoon/21610.py
@@ -1,4 +1,4 @@
-### 시간 초과
+'''
 n, m = map(int, input().split())
 maps = []
 for _ in range(n):
@@ -10,7 +10,7 @@ def get_cloud(cloud):
     new_cloud = []
     for i in range(n):
         for j in range(n):
-            if maps[i][j] > 1 and [i, j] not in cloud:
+            if maps[i][j] > 1 and not visited[i][j]:
                 new_cloud.append([i, j])
                 maps[i][j] -= 2
     return new_cloud
@@ -19,6 +19,7 @@ direction = [[], [0, -1], [-1, -1], [-1, 0], [-1, 1], [0, 1], [1, 1], [1, 0], [1
 cloud_dir = [[-1, -1], [-1, 1], [1, -1], [1, 1]]
 for _ in range(m):
     dire, cnt = map(int, input().split())
+    visited = [[False] * n for _ in range(n)]
     for i in range(len(cloud)):
         x, y = cloud[i][0] + cnt * direction[dire][0], cloud[i][1] + cnt * direction[dire][1]
         
@@ -30,7 +31,8 @@ for _ in range(m):
             y -= n
         while y < 0:
             y += n
-
+            
+        visited[x][y] = True
         maps[x][y] += 1
         cloud[i] = [x, y]
     
@@ -49,6 +51,68 @@ for _ in range(m):
         maps[clo[0]][clo[1]] += cnt
     
     cloud = get_cloud(cloud)
+
+ans = 0
+for ma in maps:
+    ans += sum(ma)
+print(ans)
+'''
+
+from collections import deque
+n, m = map(int, input().split())
+maps = []
+for _ in range(n):
+    maps.append(list(map(int, input().split())))
+
+direction = [[], [0, -1], [-1, -1], [-1, 0], [-1, 1], [0, 1], [1, 1], [1, 0], [1, -1]]
+cloud_dir = [[-1, -1], [-1, 1], [1, -1], [1, 1]]
+cloud = [[n - 1, 0], [n - 1, 1], [n - 2, 0], [n - 2, 1]]
+cloud = deque(cloud)
+
+def get_cloud():
+    for i in range(n):
+        for j in range(n):
+            if maps[i][j] > 1 and not visited[i][j]:
+                cloud.append([i, j])
+                maps[i][j] -= 2
+
+def move_cloud(dire, cnt):
+    global n
+    len_cloud = len(cloud)
+
+    for _ in range(len_cloud):
+        x, y = cloud.popleft()
+        dx, dy = direction[dire][0] * cnt, direction[dire][1] * cnt
+        nx = (x + dx) % n
+        ny = (y + dy) % n
+
+        if nx < 0:
+            nx += n
+        if ny < 0:
+            ny += n
+        
+        cloud.append([nx, ny])
+        visited[nx][ny] = True
+        maps[nx][ny] += 1
+
+def add_cloud():
+    while cloud:
+        x, y = cloud.popleft()
+        for dx, dy in cloud_dir:
+            nx, ny = x + dx, y + dy
+            if 0 <= nx < n and 0 <= ny < n:
+                if maps[nx][ny] > 0:
+                    maps[x][y] += 1
+         
+for _ in range(m):
+    dire, cnt = map(int, input().split())
+    visited = [[False] * n for _ in range(n)]
+
+    move_cloud(dire, cnt)
+
+    add_cloud()
+    
+    get_cloud()
 
 ans = 0
 for ma in maps:
