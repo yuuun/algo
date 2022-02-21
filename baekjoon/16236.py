f8992802a68488bbd31fcdf27460ab8f59440e01
yuuun <ynkim31@naver.com>
2022-02-21 11:04:37 +0900

    삼성 - 아기 상어

--- baekjoon/16236.py
+++ baekjoon/16236.py
@@ -1,18 +1,57 @@
+# 30 분 소요
 from collections import deque
 n = int(input())
-space = []
-for i in range(n):
-    tmp = list(map(int, input().split()))
-    if 9 in tmp:
-        cur = [i, tmp.index(9)]
+maps = [list(map(int, input().split())) for _ in range(n)]
 
-fish = 2
+def find_start():
+    for i in range(n):
+        for j in range(n):
+            if maps[i][j] == 9:
+                maps[i][j] = 0
+                return i, j
+
+sx, sy = find_start()
+size = 2
+ans = 0
+n_eat = 0
+
 dxy = [[-1, 0], [0, -1], [1, 0], [0, 1]]
 
-def bfs(x, y):
+def is_criteria(r, c):
+    if 0 <= r < n and 0 <= c < n:
+        return True 
+    return False
+
+while True:
+    q = deque()
+    q.append([sx, sy, 0])
     visited = [[False] * n for _ in range(n)]
-    visited[x][y] = True
-    eat = []
-    queue = deque()
-    queue.append((x, y, 0))
+    fish = []
     
+    while q:
+        x, y, dist = q.popleft()
+        for dx, dy in dxy:
+            nx, ny = x + dx, y + dy
+						# 이동 가능한 위치 탐색
+            if is_criteria(nx, ny) and size >= maps[nx][ny] and not visited[nx][ny]:
+                if maps[nx][ny] != 0 and size > maps[nx][ny]:
+                    fish.append([nx, ny, dist + 1])
+                visited[nx][ny] = True
+                q.append([nx, ny, dist + 1])
+    
+		# 먹은 물고기의 개수와 자신의 몸 크기를 비교하여 물고기의 크기를 키워주는지 확인
+    if len(fish) > 0:
+        fish = sorted(fish, key=lambda x: (x[2], x[0], x[1]))
+        x, y, dist = fish[0]
+        ans += dist
+        n_eat += 1
+        maps[x][y] = 0
+        
+        if n_eat == size:
+            size += 1
+            n_eat = 0
+        sx, sy = x, y
+    else:
+        break
+
+print(ans)
