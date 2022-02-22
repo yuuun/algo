42edf16efeb1c1b3441ae8abb8b3514a2bfc2749
yuuun <ynkim31@naver.com>
2022-02-22 12:04:55 +0900

    누적합

--- baekjoon/11660.py
+++ baekjoon/11660.py
@@ -1,20 +1,19 @@
-# 시간 초과
+# 시간 초과 -> 누적 합으로 계산
 n, m = map(int, input().split())
 
 table = [[0] * (n + 1)]
 for _ in range(n):
     table.append([0] + list(map(int, input().split())))
 
-for r in range(1, n + 1):
-    for c in range(1, n + 1):
-        table[r][c] += table[r][c - 1]
+## 누적합
+for i in range(1, n + 1):
+    for j in range(1, n):
+        table[i][j + 1] += table[i][j]
+
+for j in range(1, n + 1):
+    for i in range(1, n):
+        table[i + 1][j] += table[i][j]
 
 for _ in range(m):
     x1, y1, x2, y2 = map(int, input().split())
-    ans = 0
-    for k in range(x1, x2 + 1):
-        if y1 != 0:
-            ans -= table[k][y1 - 1]
-        ans += table[k][y2]
-        
-    print(ans)
+    print(table[x2][y2] - table[x1 - 1][y2] - table[x2][y1 - 1] + table[x1 - 1][y1 - 1])
