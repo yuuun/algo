a3061a362a33aba37936e88d44bd046902bb8091
yuuun <ynkim31@naver.com>
2022-03-25 16:02:17 +0900

    삼성 성공!

--- baekjoon/19237.py
+++ baekjoon/19237.py
@@ -1,8 +1,8 @@
-# TBD
-from collections import deque
-n, m, k = map(int, input().split()) # n: 격자, m: 상어, k: 냄새 사라지는 횟수
+import sys
+input = sys.stdin.readline
+n, m, k = map(int, input().split())  # n: 격자, m: 상어, k: 냄새 사라지는 횟수
 
-dxy = [[-1, 0], [1, 0], [0, -1], [0, 1]] # 위, 아래, 왼, 오른
+dxy = [[-1, 0], [1, 0], [0, -1], [0, 1]]  # 위, 아래, 왼, 오른
 smell = [[[] for _ in range(n)] for _ in range(n)]
 shark = {}
 for i in range(n):
@@ -10,8 +10,7 @@ for i in range(n):
     for j in range(n):
         if tmp[j] != 0:
             s = tmp[j] - 1
-            smell[i][j] = [k, s, False]
-            # shark.append((tmp[j], i, j))
+            smell[i][j] = [k, s]
             shark[s] = [i, j]
 
 for i, t in enumerate(map(int, input().split())):
@@ -24,55 +23,55 @@ for _ in range(m):
         tmp.append(list(map(lambda x: int(x) - 1, input().split())))
     dire.append(tmp)
 
-visited = [False] * m
 def reduce_smell():
     for i in range(n):
         for j in range(n):
             if smell[i][j]:
                 if smell[i][j][0] > 1:
                     smell[i][j][0] -= 1
-                    smell[i][j][2] = False
                 else:
                     smell[i][j] = []
 
 def check_direction(x, y, s, d):
+    global new_smell
     tmp = []
     for nd in dire[s][d]:
         nx, ny = x + dxy[nd][0], y + dxy[nd][1]
         if 0 <= nx < n and 0 <= ny < n:
             if smell[nx][ny] == []:
                 shark[s] = [nx, ny, nd]
-                smell[nx][ny] = [k + 1, s, False]
+                if new_smell[nx][ny]:
+                    del shark[new_smell[nx][ny][1]]
+                new_smell[nx][ny] = [k + 1, s]
                 return False
             elif smell[nx][ny][1] == s and tmp == []:
                 tmp = [nx, ny, nd]
-            elif smell[nx][ny][0] == k + 1 and not smell[nx][ny][2]: # 현 시점에서 이미 선점한 상어가 있을 경우는 제외시켜줄 것
-                # del shark[smell[nx][ny][1]]
-                remove_shark = smell[nx][ny][1]
-                shark[s] = [nx, ny, nd]
-                smell[nx][ny] = [k + 1, s, True]
-                return [remove_shark]
-    
+
     return tmp
 
+def deepcopy(arr):
+    return [[a for a in ar] for ar in arr]
+
 def move_shark():
+    global new_smell
+    new_smell = deepcopy(smell)
+    # 상어의 INDEX가 뒷 숫자이면 덮어쓰기
     for s, val in sorted(shark.items(), reverse=True):
         x, y, d = val
         tmp = check_direction(x, y, s, d)
         if tmp:
-            if len(tmp) == 1:
-                del shark[tmp[0]]
-            else:
-                nx, ny, nd = tmp
-                smell[nx][ny] = [k + 1, s, True]
-                shark[s] = [nx, ny, nd]
+            nx, ny, nd = tmp
+            new_smell[nx][ny] = [k + 1, s]
+            shark[s] = [nx, ny, nd]
+    return new_smell
 
-    
+
 ans = 1
-while ans < 1000:
-    move_shark()
+while ans <= 1000:
+    smell = move_shark()
     reduce_smell()
     if len(shark) == 1:
         break
     ans += 1
-print(ans if ans < 1000 else -1)
+
+print(ans if ans <= 1000 else -1)
