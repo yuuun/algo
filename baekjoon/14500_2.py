e2618c55c1f2601d62fe18a7ba0d4162fc05d44d
yuuun <ynkim31@naver.com>
2022-04-20 17:50:54 +0900

    testing

--- baekjoon/14500_2.py
+++ baekjoon/14500_2.py
@@ -1,21 +1,32 @@
-# TBD
 n, m = map(int, input().split())
-dxys = [[[0, 1], [0], [0]], [[0], [0, 1, 2]], [[1], [1], [0, 1]], [[0, 1, 2], [2]], [[0, 1], [1], [1]], [[2], [0, 1, 2]], [[0], [0], [0, 1]], [[0, 1, 2], [0]],
-         [[0, 1], [0, 1]],
-         [[0, 1, 2, 3]], [[0], [0], [0], [0]],
-         [[1], [0, 1], [0]], [[0], [0, 1], [1]], [[1, 2], [0, 1]], [[0, 1], [1, 2]],
-         [[0, 1, 2], [1]], [[1], [0, 1, 2]], [[0], [0, 1], [0]], [[1], [0, 1], [1]]]
-
 maps = [list(map(int, input().split())) for _ in range(n)]
-ans = []
-def cnt_score(dxy):
-    for i in range(len(dxy)):
-        for j in dxy[i]:
-            for y in range(m - len(dxy[j])):
-                return
 
-for dxy in dxys:
-    if n > len(dxy):
-        continue
-    tot = 0
-    
+dxy = [[-1, 0], [0, 1], [1, 0], [0, -1]]
+ans = 0
+visited = [[False] * m for _ in range(n)]
+max_val = max(map(max, maps))
+def dfs(x, y, idx, total):
+    global ans
+    if ans >= total + max_val * (3 - idx):
+        return
+    if idx == 3:
+        ans = max(ans, total)
+        return
+    else:
+        for dx, dy in dxy:
+            nx, ny = x + dx, y + dy
+            if 0 <= nx < n and 0 <= ny < n and not visited[nx][ny]:
+                if idx == 1:
+                    visited[nx][ny] = True
+                    dfs(x, y, idx + 1, total + maps[nx][ny])
+                    visited[nx][ny] = False
+                visited[nx][ny] = True
+                dfs(nx, ny, idx + 1, total + maps[nx][ny])
+                visited[nx][ny] = False
+            
+for x in range(n):
+    for y in range(n):
+        visited[x][y] = True
+        dfs(x, y, 0, maps[x][y])
+        visited[x][y] = False
+print(ans)
