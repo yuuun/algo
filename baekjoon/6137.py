85f4cf2263d5a211f707b146b386627de5c6f7c2
yuuun <ynkim31@naver.com>
2022-05-10 17:52:27 +0900

    two pointer

--- baekjoon/6137.py
+++ baekjoon/6137.py
@@ -3,10 +3,38 @@ n = int(input())
 s = deque()
 for _ in range(n):
     s.append(input())
+
 t = ''
-while s:
-    if s[0] > s[-1]:
-        t += s.pop()
+cnt = 0
+start, end = 0, n - 1
+while start <= end:
+    if s[start] < s[end]:
+        t += s[start]
+        start += 1
+    elif s[start] > s[end]:
+        t += s[end]
+        end -= 1
     else:
-        t += s.popleft()
+        st, se = start, end
+        flag = False
+        while st <= se:
+            if s[st] < s[se]:
+                t += s[start]
+                start += 1
+                flag = True
+                break
+            elif s[st] > s[se]:
+                t += s[end]
+                end -= 1
+                flag = True
+                break
+            else:
+                st += 1
+                se -= 1
+        if not flag:
+            t += s[start]
+            start += 1
+    cnt += 1
+    if cnt % 80 == 0:
+        t += '\n'
 print(t)
